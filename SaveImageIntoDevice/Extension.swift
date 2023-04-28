beb30f377ac91264919a74a2de0b1e6cd5905970
LimJaeHyeon9298 <limjh9298@gmail.com>
2023-04-29 00:24:25 +0900

    first commit

---
+++ SaveImageIntoDevice/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  SaveImageIntoDevice
+//
+//  Created by 임재현 on 2023/04/28.
+//
+
+import Foundation
---
+++ SaveImageIntoDevice/ImageFileManager.swift
@@ -0,0 +1,8 @@
+//
+//  ImageFileManager.swift
+//  SaveImageIntoDevice
+//
+//  Created by 임재현 on 2023/04/28.
+//
+
+import Foundation
